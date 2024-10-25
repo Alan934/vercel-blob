da5dc7b9be4719806235a1430ad71e98b73f5799
Alan934 <sanjurjoalan77@gmail.com>
2024-10-25 18:52:07 -0300

    all files in grid and responsive

--- src/app/all-files/page.tsx
+++ src/app/all-files/page.tsx
@@ -12,8 +12,8 @@ export default async function AllFilesPage() {
     } else {
         return (
             <div>
-                <h1 className="text-2xl font-bold mb-4">Files</h1>
-                <div className="grid grid-cols-5 gap-4">
+                <h1 className="text-2xl font-bold mb-4 text-center">Files</h1>
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 justify-items-center">
                     {blobs.blobs.map((blob, i) => (
                         <div
                             key={blob.pathname + i}
