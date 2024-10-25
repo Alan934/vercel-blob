815c5304e8db6780e60418791e838f66dedbd2ec
Alan934 <sanjurjoalan77@gmail.com>
2024-10-25 15:02:08 -0300

    preview img

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -1,10 +1,14 @@
 "use client"; 
 import { useState } from "react";
+import Image from "next/image";
 
 export default function UploadForm(){
     const [ file, setFile] = useState<File | null>(null);
+    const [ preview, setPreview ] = useState<string | null>(null);
+    const [ inProgess, setInProgesss ] = useState(false);
 
     const handleSubmit = async ( e: React.FormEvent ) => {
+        setInProgesss(true);
         e.preventDefault();
 
         const formData = new FormData();
@@ -14,6 +18,10 @@ export default function UploadForm(){
             method: 'POST',
             body: formData
         });
+
+        const data = await response.json();
+        setPreview(data.url);
+        setInProgesss(false);
     };
 
     const styles = {
@@ -25,7 +33,16 @@ export default function UploadForm(){
         <form onSubmit={handleSubmit}>
             <label htmlFor="fileUpload">Choose a file to upload: </label>
             <input className={styles.input} id="fileUpload" type="file" onChange={(e) => setFile(e.target.files?.item(0) || null )} />
-            <button type="submit">Upload</button>
+            <button className="
+            bg-blue-500 text-white p-2 rounded w-full hover:bg-blue-600" 
+            type="submit">{ inProgess ? "Uploading..." : "Upload" }</button
+            >
+
+            {preview && (
+                <div>
+                    <Image src={preview} alt="test" width={1200} height={1200} />
+                </div>
+            )}
         </form>
     );
 }
