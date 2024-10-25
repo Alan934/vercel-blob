ff2351cdc83434dc206e9a062d243cb08e9d7177
Alan934 <sanjurjoalan77@gmail.com>
2024-10-25 19:50:23 -0300

    test deploy

--- src/app/components/deleteButton.tsx
+++ src/app/components/deleteButton.tsx
@@ -1,36 +1,35 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+// deleteButton.tsx
+import React from "react";
 
-export function DeleteButton({url}: {url: string} ){
-    const [isLoading, setIsLoading] = useState(false);
-    const router = useRouter();
+interface DeleteButtonProps {
+    url: string;
+    onBlobChange: () => void; // Incluye esta propiedad
+}
 
-    const handleClick = async () => {
-        setIsLoading(true);
+export const DeleteButton: React.FC<DeleteButtonProps> = ({ url, onBlobChange }) => {
+    const handleDelete = async () => {
         try {
-            const response = await fetch('/api/file/' + encodeURIComponent(url), {
-                method: 'DELETE',
+            const response = await fetch(url, {
+                method: 'DELETE', // Método HTTP para eliminar
                 headers: {
-                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${process.env.NEXT_PUBLIC_BLOB_READ_WRITE_TOKEN}`, // Asegúrate de que el token esté correcto
                 },
             });
-    
+
             if (!response.ok) {
-                throw new Error('Failed to delete file.');
+                throw new Error(`Error al eliminar el blob: ${response.statusText}`);
             }
-    
-            setIsLoading(false);
-            router.refresh();
+
+            onBlobChange(); // Llama a la función para actualizar la lista de blobs
         } catch (error) {
-            console.error('Error deleting file:', error);
-            setIsLoading(false);
+            console.error("Error deleting blob:", error);
+            // Aquí puedes manejar el error si lo deseas
         }
     };
 
     return (
-        <button onClick={handleClick} className="bg-red-500 text-white p-2 rounded hover:bg-red-600" disabled={isLoading}>
-            {isLoading? 'Deleting...' : 'Delete'}
+        <button onClick={handleDelete} className="bg-red-500 text-white px-2 py-1 rounded">
+            Delete
         </button>
-    )
-}
+    );
+};
